f69ad4a84c5bd3ca5f6855227f0b90c406e61d34
liangwei0101 <liangwei0101@users.noreply.github.com>
2018-07-06 20:02:45 +0800

    增加代码

--- 前端/study/src/components/one.vue
+++ 前端/study/src/components/one.vue
@@ -1,10 +1,17 @@
 <template>
-    <Table border :columns="columns7" :data="data6"></Table>
+  <div>
+    <Input v-model="value13" >
+        <span slot="prepend">请输入名字或者编号查询</span>
+        <Button @click="search" slot="append" icon="ios-search"/></Button>
+    </Input>
+    <Table border :loading="loading" :columns="columns7" :data="data6"></Table>
+  </div>
 </template>
 <script>
     export default {
         data () {
             return {
+                loading: true,
                 columns7: [
                     {
                         title: 'Name',
@@ -68,22 +75,22 @@
                 ],
                 data6: [
                     {
-                        name: 'John Brown',
+                        name: '梁聪聪',
                         age: 18,
                         address: 'New York No. 1 Lake Park'
                     },
                     {
-                        name: 'Jim Green',
+                        name: '梁伟',
                         age: 24,
                         address: 'London No. 1 Lake Park'
                     },
                     {
-                        name: 'Joe Black',
+                        name: '李小龙',
                         age: 30,
                         address: 'Sydney No. 1 Lake Park'
                     },
                     {
-                        name: 'Jon Snow',
+                        name: '小生',
                         age: 26,
                         address: 'Ottawa No. 2 Lake Park'
                     },
@@ -120,6 +127,12 @@
                 ]
             }
         },
+        mounted (){ // 加载中事件
+            // 定时关闭缓冲标志（1.5秒后）
+            setTimeout(()=>{
+                this.loading = false
+            },1500)
+        },
         methods: {
             show (index) {
                 this.$Modal.info({
@@ -129,6 +142,9 @@
             },
             remove (index) {
                 this.data6.splice(index, 1);
+            },
+            search () {
+                alert('我是测试')
             }
         }
     }

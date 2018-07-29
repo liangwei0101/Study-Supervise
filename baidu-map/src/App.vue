55a1b86f012ccce5b88f463b4496168a7c5bfe05
liangcongcong <liangcongcong0930@outlook.com>
2018-07-29 20:09:05 +0800

    打包

--- baidu-map/src/App.vue
+++ baidu-map/src/App.vue
@@ -2,11 +2,14 @@
 
 <baidu-map class="map" :center="{lng: 115.863435, lat: 28.692497}" :zoom="12" :scroll-wheel-zoom="true">
 <bm-map-type :map-types="['BMAP_NORMAL_MAP', 'BMAP_HYBRID_MAP']" anchor="BMAP_ANCHOR_TOP_LEFT"></bm-map-type>
-<label>关键词：<input v-model="keyword"></label>
+<label>地区检索关键词：<input v-model="keyword"></label>
   <label>地区：<input v-model="location"></label>
   <bm-view class="map1" :center="{lng: 115.863435, lat: 28.692497}" :zoom="12" :scroll-wheel-zoom="true"></bm-view>
-
+  <bm-local-search :keyword="keyword" :auto-viewport="true" :location="location"></bm-local-search>
+<label>公交线路号：<input v-model="busKeyWord"></label>
   <bm-bus :keyword="busKeyWord" :auto-viewport="true" :location="location"/>
+  <label>起点：<input v-model="start"></label><label>终点：<input v-model="end"></label>
+  <bm-transit :start="start"  :end="end" :auto-viewport="true" :location="location"></bm-transit>
   </baidu-map>
 </template>
 
@@ -15,8 +18,10 @@ export default {
 data () {
     return {
       location: '南昌',
-      keyword: '百度',
-      busKeyWord:'232'
+      keyword: '',
+      busKeyWord:'',
+      start:'',
+      end:''
     }
   }
 }

5159357dc2a94adeaea5907a70f04d0a671b691b
liangcongcong <liangcongcong0930@outlook.com>
2018-07-29 15:59:05 +0800

    vuemap

--- baidu-map/src/App.vue
+++ baidu-map/src/App.vue
@@ -1,11 +1,24 @@
 <template>
- <baidu-map class="map" :center="{lng: 116.404, lat: 39.915}" :zoom="15" :scroll-wheel-zoom="true">
-  </baidu-map>
+
+<baidu-map class="map" :center="{lng: 115.863435, lat: 28.692497}" :zoom="12" :scroll-wheel-zoom="true">
+<bm-map-type :map-types="['BMAP_NORMAL_MAP', 'BMAP_HYBRID_MAP']" anchor="BMAP_ANCHOR_TOP_LEFT"></bm-map-type>
+<label>关键词：<input v-model="keyword"></label>
+  <label>地区：<input v-model="location"></label>
+  <bm-view class="map1" :center="{lng: 115.863435, lat: 28.692497}" :zoom="12" :scroll-wheel-zoom="true"></bm-view>
+  <bm-local-search :keyword="keyword" :auto-viewport="true" :location="location"></bm-local-search>
+  <bm-bus keyword="520" :auto-viewport="true" location="南昌"></bm-bus>
+  <bm-transit start="江西财经大学麦庐园校区" end="八一广场" :auto-viewport="true" location="南昌"></bm-transit>
+</baidu-map>
 </template>
 
 <script>
 export default {
-
+data () {
+    return {
+      location: '南昌',
+      keyword: '百度'
+    }
+  }
 }
 </script>
 
@@ -13,5 +26,9 @@ export default {
 .map {
   width: 100%;
   height: 600px;
+}
+.map1 {
+  width: 100%;
+  height: 400px;
 }
 </style>

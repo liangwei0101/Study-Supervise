f8c35df431d204eafb1a42b5dfb440ec8d535a58
liangcongcong <liangcongcong0930@outlook.com>
2018-11-19 18:57:41 +0800

    Java实现计算器

--- Java应用程序演示/src/计算器/Calculator.java
+++ Java应用程序演示/src/计算器/Calculator.java
@@ -1,4 +1,4 @@
-
+package 计算器;
 
 public class Calculator {
     public static  void main(String[] args)
--- Java应用程序演示/src/计算器/DemoWindow.java
+++ Java应用程序演示/src/计算器/DemoWindow.java
@@ -1,3 +1,5 @@
+package 计算器;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;

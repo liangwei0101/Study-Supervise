73de9a5c5e6a670bcdde521cbab514bed83fec2c
liangcongcong <liangcongcong0930@outlook.com>
2019-01-05 11:00:48 +0800

    dormitory1

---
+++ dormitory/src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+    <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                    <div class="layout-logo"></div>
+                    <div class="layout-nav">
+                        <MenuItem name="1">
+                            <Icon type="ios-navigate"></Icon>
+                         <router-link to="dormitory">  宿舍管理</router-link>
+                        </MenuItem>
+                      
+                          <MenuItem name="2">
+                            <Icon type="ios-keypad"></Icon>
+                        <router-link to="electric"> 电费管理</router-link>
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <Icon type="ios-analytics"></Icon>
+                           <router-link to="student"> 人员管理</router-link>
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <Icon type="ios-paper"></Icon>
+                           <router-link to="repair"> 报修管理</router-link>
+                        </MenuItem>
+                    </div>
+                </Menu>
+            </Header>
+            <Content :style="{padding: '0 50px'}">
+                <Breadcrumb :style="{margin: '20px 0'}">
+                    <BreadcrumbItem>Home</BreadcrumbItem>
+                    <BreadcrumbItem>Components</BreadcrumbItem>
+                    <BreadcrumbItem>Layout</BreadcrumbItem>
+                </Breadcrumb>
+                <Card>
+                    <div style="min-height: 620px;">
+                       <router-view/>
+                    </div>
+                </Card>
+            </Content>
+            <Footer class="layout-footer-center"></Footer>
+        </Layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+   data () {
+            return {
+                isCollapsed: false
+            };
+        },
+        computed: {
+            menuitemClasses: function () {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            }
+        }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+}
+.layout-logo{
+    width: 100px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
+}
+.layout-nav{
+    width: 520px;
+    margin: 0 auto;
+    margin-right: 20px;
+}
+.layout-footer-center{
+    text-align: center;
+}
+</style>
---
+++ dormitory/src/components/common/main.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="Mainid">
+      <P>   江财学生宿舍管理系统</P>
+        <!-- <img src="./01.png">-->
+    </div>
+</template>
+<style scoped>
+#Mainid P{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 50px;
+    color: dimgrey;
+    text-align: center;
+    
+}
+</style>
